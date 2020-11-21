476c5835e1b1b65497d1faf2cc8b933bd6049fe4
Alexandre <alexandre.girbal.pro@gmail.com>
2020-11-21 18:47:42 +0100

    debug tests

--- src/test/java/polytech/covidalert/TestUserController.java
+++ src/test/java/polytech/covidalert/TestUserController.java
@@ -1,7 +1,7 @@
 package polytech.covidalert;
 
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +10,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import polytech.covidalert.controllers.UserController;
 import polytech.covidalert.models.User;
-import polytech.covidalert.models.UserRepository;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(controllers = UserController.class)
 public class TestUserController {
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -26,23 +29,22 @@ public class TestUserController {
     @MockBean
     private UserController userController;
 
-    @MockBean
-    private UserRepository userRepository;
-
 
     @Test
     @DisplayName("Test Create")
-    public void userControllershouldReturnUser() throws Exception{
+    public void userControllerShouldReturnUser() throws Exception{
 
-       User user1= new User("jean","Neymar","20/08/2010","jeanmrlzefzefezf@gmail.com","0645789654","azerty");
-       doReturn(user1).when(userRepository).save(any());
+       User user1= new User("jean","Neymar","20/08/2010","jean@gmail.com","0645789654","azerty");
+       doReturn(user1).when(userController).getUserByEmail(user1.getEmail());
+       String result = mockMvc.perform(get("/covidalert/api/users/{id}",user1.getEmail()))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.email", is(user1.getEmail())))
+               .andExpect(jsonPath("$.first_name", is(user1.getFirst_name())))
+               .andExpect(jsonPath("$.last_name", is(user1.getLast_name())))
+               .andReturn().getResponse().getContentAsString();
+        System.out.println("\nResult:");
+       System.out.println(result);
 
-       User returnedUser = userController.create(user1);
-        System.out.println(user1);
-        System.out.println(returnedUser);
-
-       userController.create(user1);
-       Assertions.assertEquals(user1, returnedUser, "Create should return the user");
     }
 
 }
